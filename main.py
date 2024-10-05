37368d58b2335738617731fcf632d085ce91ca89
CPMAizal <ayankhan12246644@gmail.com>
2024-10-05 09:38:11 +0500

    Update main.py

--- main.py
+++ main.py
@@ -50,11 +50,13 @@ def banner(console):
     colorful_text = gradient_text(brand_name, colors)
     console.print(colorful_text)
     console.print("[bold][red]==================================================================[/red][/bold]")
-    console.print("\t  [bold] [green]                   👑 𝐂𝐏𝐌 𝐀𝐈𝐙𝐀𝐋 🤴")
+    console.print("\t  [bold] [green]                   👑 𝐂𝐏𝐌 𝐀𝐈𝐙𝐀𝐋 𝐊𝐔𝐑𝐃𝐈𝐒𝐇")
     console.print("   [bold][red] 𝐒𝐇𝐀𝐑𝐈𝐍𝐆 𝐓𝐇𝐄 𝐀𝐂𝐂𝐄𝐒𝐒 𝐊𝐄𝐘 𝐈𝐒 𝐍𝐎𝐓 𝐀𝐋𝐋𝐎𝐖𝐄𝐃 𝐀𝐍𝐃 𝐖𝐈𝐋𝐋 𝐁𝐄 𝐁𝐋𝐎𝐂𝐊𝐄𝐃 [/bold][red]")
-    console.print("   [bold][green] 𝐋𝐎𝐆𝐎𝐔𝐓 𝐅𝐑𝐎𝐌 𝐂𝐏𝐌 𝐁𝐄𝐅𝐎𝐑𝐄 𝐔𝐒𝐈𝐍𝐆 𝐓𝐇𝐈𝐒 𝐓𝐎𝐎𝐋 [/bold][green]")
+    console.print("   [bold][yellow]         𝐋𝐎𝐆𝐎𝐔𝐓 𝐅𝐑𝐎𝐌 𝐂𝐏𝐌 𝐁𝐄𝐅𝐎𝐑𝐄 𝐔𝐒𝐈𝐍𝐆 𝐓𝐇𝐈𝐒 𝐓𝐎𝐎𝐋 [/bold][yellow]")
     console.print("   [bold][white] 𝐂𝐎𝐍𝐓𝐀𝐂𝐓 𝐖𝐈𝐓𝐇 𝐌𝐄 𝐓𝐎 𝐁𝐔𝐘 𝐀𝐍 𝐔𝐍𝐋𝐈𝐌𝐈𝐓𝐄𝐃 𝐁𝐀𝐋𝐀𝐍𝐂𝐄 𝐓𝐆 @𝐈𝐭𝐳𝐀𝐢𝐳𝐚𝐥 [/bold][white]")
-    console.print("[bold][red]==================================================================[/red][/bold]")
+    console.print(f"             [bold blue]𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 𝐁𝐎𝐓 [/bold blue]: [bold blue]@cpmsalebot[/bold blue] 𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦 [bold blue]@cpmtool[/bold blue]") 
+    
+    console.print("[bold][purple]==================================================================[/purple][/bold]")
 
 def load_player_data(cpm):
     response = cpm.get_player_data()
@@ -109,7 +111,7 @@ def interpolate_color(start_color, end_color, fraction):
     interpolated_rgb = tuple(int(start + fraction * (end - start)) for start, end in zip(start_rgb, end_rgb))
     return "{:02x}{:02x}{:02x}".format(*interpolated_rgb)
 
-#cpmtool @abobalol228 @cpmtool
+
 
 def rainbow_gradient_string(customer_name):
     modified_string = ""
