df4415f6fefa5438397910c1892da087a3768ae7
CPMAizal <ayankhan12246644@gmail.com>
2024-10-21 14:56:12 +0500

    Update and rename cpmtool.py to cpmgala.py

--- cpmgala.py
+++ cpmgala.py
@@ -1,8 +1,8 @@
 import requests
 from time import sleep
-BASE_URL: str = "https://maroon-echidna-796942.hostingersite.com/api"
+BASE_URL: str = "https://red-viper-815239.hostingersite.com/api" 
 
-class CPMtool:
+class CPMGala:
 
     def __init__(self, access_key) -> None:
         self.auth_token = None
